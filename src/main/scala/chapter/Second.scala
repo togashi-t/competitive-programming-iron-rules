26528e27848a84139a35b3d7a04a2d7c0a590bdf
togashi-t <saladwithsalad@yahoo.co.jp>
2023-05-03 12:04:05 +0900

    twoDimensionalSum

--- src/main/scala/chapter/Second.scala
+++ src/main/scala/chapter/Second.scala
@@ -48,5 +48,42 @@ object Second extends App {
   }
 
 
+  // 二次元の累積和(1)
+  def twoDimensionalSum = {
+    // 標準入力から必要な情報を取得
+    val List(height, width) = StdIn.readLine.split(" ").map(_.toInt).toList
+    val matrix = (1 to height).toVector.map { _ =>
+      StdIn.readLine().split(" ").map(_.toInt).toVector
+    }
+    val questionCount = StdIn.readInt()
+    val questionNumbersList = (1 to questionCount).toList.map { _ =>
+      StdIn.readLine().split(" ").map(_.toInt).toList
+    }
+
+    // 二次元の累積和の算出。元のmatrixから行、列がそれぞれ1つ増える。最初の行と列の全要素の値は0となる。
+    val cumulativeMatrix = {
+      // まずは横方向の累積和を求める
+      val horizontalCumulativeMatrix = matrix.map { rowNumbers =>
+        rowNumbers.scanLeft(0)(_ + _) // 先頭に0が追加されて、元よりも長さが1増える
+      }
+      // 横方向の累積和を求めたmatrixを基にして縦方向の累積和を求めることで、完成形の累積和のmatrixを求める
+      // 最初の行に0を追加
+      val extendedMatrix = Vector.fill(1, width + 1)(0) ++ horizontalCumulativeMatrix
+      // 2行目以降の行から順番に、各要素に対して1行上の同一列の値を加算する
+      (1 to height).foldLeft(extendedMatrix) { case (tmpMatrix, rowIndex) =>
+        tmpMatrix.updated(
+          rowIndex,
+          (0 to width).map(colIndex => tmpMatrix(rowIndex - 1)(colIndex) + tmpMatrix(rowIndex)(colIndex)).toVector
+        )
+      }
+    }
+
+    // 問題への回答
+    questionNumbersList.map { case (startRowIndex :: startColIndex :: endRowIndex :: endColIndex :: Nil) =>
+      cumulativeMatrix(endRowIndex)(endColIndex) + cumulativeMatrix(startRowIndex - 1)(startColIndex - 1) - cumulativeMatrix(startRowIndex - 1)(endColIndex) - cumulativeMatrix(endRowIndex)(startColIndex - 1)
+    }
+  }
+
+
 }
 
