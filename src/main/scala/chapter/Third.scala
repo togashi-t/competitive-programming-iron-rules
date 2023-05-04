ff477763fae92ce9e6daa66f653fa58a9f3c7f70
togashi-t <saladwithsalad@yahoo.co.jp>
2023-05-04 15:22:25 +0900

    printer

--- src/main/scala/chapter/Third.scala
+++ src/main/scala/chapter/Third.scala
@@ -26,4 +26,33 @@ object Third {
     searchIndexFunc(numbers, target, 0, initLength - 1) + 1 // indexは0スタートなので1を加算して何番目という数値と合わせる
   }
 
+
+  // 答えで二分探索
+  def printer = {
+    val List(count, targetPrintNumber) = StdIn.readLine().split(" ").map(_.toLong).toList
+    val intervals = StdIn.readLine().split(" ").map(_.toLong).toList
+
+    // 経過時間から印刷枚数を返す
+    def printNumberFromMinutes(minutes: Long): Long = intervals.map(minutes / _).sum
+
+    // 経過時間での印刷枚数が目標印刷枚数を達成しているかを返す
+    def isGreaterThanTargetPrintNumber(minutes: Long): Boolean = printNumberFromMinutes(minutes) >= targetPrintNumber
+
+    @tailrec
+    def searchMinutesFunc(minMinutes: Long, maxMinutes: Long): Long = {
+      if (maxMinutes == minMinutes) { // 調査対象時間の範囲が尽きた場合
+        minMinutes
+      } else {
+        val middleMinutes = (minMinutes + maxMinutes) / 2
+        if (isGreaterThanTargetPrintNumber(middleMinutes)) { // 印刷枚数が目標以上の場合
+          searchMinutesFunc(minMinutes, middleMinutes)
+        } else { // 印刷枚数が目標を下回る場合。すなわちmiddleMinutesでは時間不足の場合
+          searchMinutesFunc(middleMinutes + 1, maxMinutes)
+        }
+      }
+    }
+
+    searchMinutesFunc(1, 1000000000)
+  }
+
 }
