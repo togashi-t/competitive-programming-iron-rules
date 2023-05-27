47ae10c034663900c864b29799388875db359d49
togashi-t <saladwithsalad@yahoo.co.jp>
2023-05-27 11:32:56 +0900

    blackCompany1

--- src/main/scala/chapter/Sixth.scala
+++ src/main/scala/chapter/Sixth.scala
@@ -25,4 +25,25 @@ object Sixth {
     fromStationMinutesVector.sum * m + b * (n * m) + fromBusStopMinutesVector.sum * n
   }
 
+
+  // 上限値を考える
+  def blackCompany1 = {
+    val scanner = new java.util.Scanner(System.in)
+    val List(d, n) = List.fill(2)(scanner.nextInt())
+    val lrhList = (for (_ <- 1 to n) yield List.fill(3)(scanner.nextInt())).toList
+
+    // 各日の上限時間を整理するArray。初期値は24時間としておく。
+    // ただし、後段の処理時に何日目の数値とindexが一致していた方がわかりやすいので、先頭に0の要素を入れている。
+    val dayMaxHourArray = Array.tabulate(d + 1)(i => if (i == 0) 0 else 24)
+
+    lrhList.foreach { case (l :: r :: h :: Nil) =>
+      for (x <- l to r) {
+        val minHour = math.min(dayMaxHourArray(x), h)
+        dayMaxHourArray(x) = minHour
+      }
+    }
+
+    dayMaxHourArray.sum
+  }
+
 }
