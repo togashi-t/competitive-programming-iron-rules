48f0c308ec56dc8bd5e4fa642fcebf0c9a15bfc4
togashi-t <saladwithsalad@yahoo.co.jp>
2023-05-27 10:58:31 +0900

    travel2

--- src/main/scala/chapter/Sixth.scala
+++ src/main/scala/chapter/Sixth.scala
@@ -12,4 +12,17 @@ object Sixth {
     res
   }
 
+
+  // 足された回数を考える
+  // 主客転倒テクニック。視点を変えて楽に数え上げをしよう、ということ。
+  // 具体的に言うと「各事象がどれだけ合計に寄与、貢献しているかを考える」ということ。
+  def travel2 = {
+    val scanner = new java.util.Scanner(System.in)
+    val Vector(n, m, b) = Vector.fill(3)(scanner.nextLong())
+    val fromStationMinutesVector = Vector.fill(n.toInt)(scanner.nextLong())
+    val fromBusStopMinutesVector = Vector.fill(m.toInt)(scanner.nextLong())
+
+    fromStationMinutesVector.sum * m + b * (n * m) + fromBusStopMinutesVector.sum * n
+  }
+
 }
